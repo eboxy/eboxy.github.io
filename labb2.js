13836ba3942d736bc041fef018c2c9ad3baf0eb6
eboxy55 <eboxy55@gmail.com>
2018-02-28 21:22:45 +0100

    testing js

--- labb2.js
+++ labb2.js
@@ -1,3 +1,23 @@
-function msg(){
- alert("welcome to Litterary memories");
+function myFunction() {
+    let pee = document.getElementById("dunk");
+    pee.style.backgroundColor = "hotpink";
+
+    let el1 = document.getElementById('nisse'); // hitta rätt element
+    let el2 = document.createElement('span');
+    el2.innerHTML = ' - HACKED';
+    el2.style.fontWeight = 'bold';
+
+    el1.appendChild(el2);
+
+  let btn = document.getElementById("btn");
+    btn.addEventListener("click", callback);
+
 }
+
+
+//alert("welcome to Litterary memories");
+
+let callback = function(event) {
+//console.log('Something happened');
+alert("tjoohoo  i superryjmden");
+};
