bdee8f29bee5435397a17ff03ecd784cc262ec62
eboxy55 <eboxy55@gmail.com>
2018-03-06 13:50:48 +0100

    updates

--- labb2.js
+++ labb2.js
@@ -1,5 +1,57 @@
 
 
+//hämta api-nyckel
+
+window.addEventListener('load', function() {
+
+	let buttonApiKey = document.getElementById('btnApiKey');
+				buttonApiKey.addEventListener('click', function() {
+
+console.log('Det fungerar!');
+
+let urlApiKey = 'https://www.forverkliga.se/JavaScript/api/crud.php';
+let opApiKey = 'requestKey';
+
+let finalUrlApiKey = `${urlApiKey}?${opApiKey}`;
+
+console.log('Hämtar data från: ' + finalUrlApiKey);
+fetch(finalUrlApiKey)
+.then( responseApiKey => {
+	console.log('Svar från servern:', responseApiKey);
+	return responseApiKey.json();
+} )
+.then(function(objApiKey){
+
+  console.log('svar som objekt',objApiKey);
+	let outputApiKey = document.getElementById('responseApiKey');
+	let statusApiKey = document.getElementById('statusApiKey');
+
+statusApiKey.innerHTML = "<strong>Status:</strong> " + objApiKey.status + "<br><br>"
++ "<strong>Meddelande:</strong> " + objApiKey.message;
+
+
+//lagra api-nyckel i konstant
+var storageApiKey = objApiKey.key;
+
+ outputApiKey.innerHTML = storageApiKey;
+
+})
+
+
+.catch( messageApiKey => {
+	console.log('Något gick fel: ' + messageApiKey);
+});
+
+});
+
+});
+
+
+//................................................
+//................................................
+
+
+
 //hämta boklistan
 
 window.addEventListener('load', function() {
@@ -11,7 +63,11 @@ console.log('Det fungerar!');
 
 let url = 'https://www.forverkliga.se/JavaScript/api/crud.php';
 let op = 'select';
-let key = 'fzJ8Z';
+//let key = 'fzJ8Z';
+
+
+let key = storageApiKey;
+//storageApiKey
 
 let finalUrl = `${url}?op=${op}&key=${key}`;
 console.log('Hämtar data från: ' + finalUrl);
